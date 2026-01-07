61e772f6d44b60e802189b4a528fce389d5e5b68
AbdelrahmanBadr74 <abdobadr868@gmail.com>
2026-01-07 07:28:07 +0200

    update cors

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,6 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:4200",
-      "https://thinkflow-api.vercel.app/",
       "http://localhost:3000",
     ],
     credentials: true,
